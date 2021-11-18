ef9b0f87f096a092b352425121630f734892679c
Qoiz12 <77888612+qoiz12@users.noreply.github.com>
2021-11-18 09:42:03 +0000

    Add files via upload

--- you.js
+++ you.js
@@ -1,18 +1,39 @@
 /* [Oct 2021] Added to comply with strict browser policies. */
+document.addEventListener('click', musicPlay);
+
+function musicPlay() {
+    var audio = document.getElementById('youare-audio');
+	var micon = document.getElementById('youare-micon');
+	
+	micon.addEventListener('click', musicPlay);
+	
+	if (audio.duration > 0 && audio.paused) {
+		audio.play();
+		micon.src = "images/speaker.png";
+	}
+	else {
+		audio.pause();
+		audio.currentTime = 0;
+		
+		micon.src = "images/speakerm.png";
+	}
+	
+	document.removeEventListener('click', musicPlay);
+}
+
+var faudio = new Audio('youare.mp3')
+
+faudio.addEventListener('timeupdate', function() {
+	console.log('TimeUpdate invoked.');
+
+    if (this.currentTime > this.duration - .45) {
+        this.currentTime = 0;
+        this.play();
+    }
+}
 );
-var source = "audio/youare.mp3"
- var audio = document.createElement("audio");
- //
- audio.autoplay = true;
- //
- audio.load()
- audio.addEventListener("load", function() { 
-     audio.play(); 
- }, true);
- audio.src = source;
-function proCreate() {	
-	for (var i = 0; i < 5; i++) {
-		openWindow('lol.html');
+/* [Oct 2021] End part. */
+
 function bookmark() {
 	if ((navigator.appName == "Microsoft Internet Explorer") && (parseInt(navigator.appVersion) >= 4)) {
 		var url = "lol.html";
@@ -87,7 +108,7 @@ window.onload = function () {
 	flagRun = 1;
 	
 	playBall();
-	bookmark(); // Internet Explorer only (what a piece of Bleep)
+	bookmark(); // Internet Explorer only (what a piece of shit)
 	
 	return true;
 }
